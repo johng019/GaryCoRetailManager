fa21d99cc15ae72fd2cecbc35407d8d891de5bb9
gary0958 <gljfb@yahoo.com>
2019-02-18 16:24:42 -0500

    Register DB Tables
    The tables for the basic register system.

---
+++ GRMData/dbo/Tables/Sale.sql
@@ -0,0 +1,9 @@
+﻿CREATE TABLE [dbo].[Sale]
+(
+	[Id] INT NOT NULL PRIMARY KEY IDENTITY, 
+    [CashierId] NVARCHAR(128) NOT NULL, 
+    [SaleDate] DATETIME2 NOT NULL, 
+    [SubTotal] MONEY NOT NULL, 
+    [Tax] MONEY NOT NULL DEFAULT 0, 
+    [Total] MONEY NOT NULL 
+)
