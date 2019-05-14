b98744c38e1cf3697a55182fb482025f72e8ccac
gary0958 <gljfb@yahoo.com>
2019-05-14 14:05:19 -0400

    Get Add To Cart Working
    Adds the functionality to add items to the cart, get the subtotal, and update quantities.

---
+++ GRMDesktopUI.Library/Models/CartItemModel.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GRMDesktopUI.Library.Models
+{
+    public class CartItemModel
+    {
+        public ProductModell Product { get; set; }
+
+        public int QuantityInCart { get; set; }
+
+        public string DisplayText
+        {
+            get
+            {
+                return $"{Product.ProductName} ({QuantityInCart})";
+            }
+        }     
+    }
+}
--- GRMDesktopUI/ViewModels/SalesViewModel.cs
+++ GRMDesktopUI/ViewModels/SalesViewModel.cs
@@ -28,12 +28,12 @@ namespace GRMDesktopUI.ViewModels
         public async Task LoadProducts()
         {
             var productList = await _productEndPoint.GetAll();
-            Products = new BindingList<ProductModel>(productList);
+            Products = new BindingList<ProductModell>(productList);
         }
 
-        private BindingList<ProductModel> _products;
+        private BindingList<ProductModell> _products;
 
-        public  BindingList<ProductModel> Products
+        public  BindingList<ProductModell> Products
         {
             get { return _products; }
             set
@@ -43,9 +43,22 @@ namespace GRMDesktopUI.ViewModels
             }
         }
 
-        private BindingList<ProductModel> _cart;
+        private ProductModell _selectedProduct;
 
-        public BindingList<ProductModel> Cart
+        public ProductModell SelectedProduct
+        {
+            get { return _selectedProduct; }
+            set
+            {
+                _selectedProduct = value;
+                NotifyOfPropertyChange(() => SelectedProduct);
+                NotifyOfPropertyChange(() => CanAddToCart);
+            }
+        }
+
+        private BindingList<CartItemModel> _cart = new BindingList<CartItemModel>();
+
+        public BindingList<CartItemModel> Cart
         {
             get { return _cart; }
             set
@@ -55,7 +68,7 @@ namespace GRMDesktopUI.ViewModels
             }
         }
 
-        private int _iItemQuantity;
+        private int _iItemQuantity = 1;
 
         public int  ItemQuantity
         {
@@ -64,6 +77,7 @@ namespace GRMDesktopUI.ViewModels
             {
                 _iItemQuantity = value;
                 NotifyOfPropertyChange(() => ItemQuantity);
+                NotifyOfPropertyChange(() => CanAddToCart);
             }
         }
 
@@ -71,8 +85,13 @@ namespace GRMDesktopUI.ViewModels
         {
             get
             {
-                //ToDo - Replace with calculation
-                return "$0.00";
+                decimal subTotal = 0;
+
+                foreach (var item in Cart)
+                {
+                    subTotal += (item.Product.RetailPrice * item.QuantityInCart);
+                }
+                return subTotal.ToString("C");
             }
         }
 
@@ -100,16 +119,40 @@ namespace GRMDesktopUI.ViewModels
             {
                 bool output = false;
 
-               //Make sure something is selected
-               //Make sure there is an item quantity
-
+                //Make sure something is selected
+                //Make sure there is an item quantity
+                if (ItemQuantity > 0 && SelectedProduct?.QuantityInStock >= ItemQuantity)
+                {
+                    output = true;
+                }
                 return output;
             }
         }
 
         public void AddToCart()
         {
-
+            CartItemModel existingItem = Cart.First(x => x.Product == SelectedProduct);
+            if (existingItem != null)
+            {
+                existingItem.QuantityInCart += ItemQuantity;
+                //Hack - There should be a better way of refreshing the cart display
+                Cart.Remove(existingItem);
+                Cart.Add(existingItem);
+            }
+            else
+            {
+                CartItemModel item = new CartItemModel
+                {
+                    Product = SelectedProduct,
+                    QuantityInCart = ItemQuantity
+                };
+                Cart.Add(item);
+            }
+            
+            SelectedProduct.QuantityInStock -= ItemQuantity;
+            ItemQuantity = 1;
+            NotifyOfPropertyChange(() => SubTotal);
+            //NotifyOfPropertyChange(() => Cart);
         }
 
         public bool CanRemoveFromCart
@@ -126,7 +169,7 @@ namespace GRMDesktopUI.ViewModels
 
         public void RemoveFromCart()
         {
-
+            NotifyOfPropertyChange(() => SubTotal);
         }
 
         public bool CanCheckOut
