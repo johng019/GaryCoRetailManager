bd570acb756d09b2199fb5a1379bf0204da74e74
gary0958 <gljfb@yahoo.com>
2019-08-05 13:53:32 -0400

    Fix identity lookup
    Used Scope_Identity now

--- GRMData/dbo/Stored Procedures/spSale_Insert.sql
+++ GRMData/dbo/Stored Procedures/spSale_Insert.sql
@@ -12,6 +12,6 @@ begin
 	insert into dbo.Sale(CashierId,SaleDate,SubTotal,Tax,Total)
 	values (@CashierId,@SaleDate,@SubTotal,@Tax,@Total);
 
-	select @Id =@@IDENTITY;
+	select @Id = Scope_IDENTITY();
 end
 
