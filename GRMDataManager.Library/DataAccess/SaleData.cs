9e172fe8c92591d681cda92eb55f32ad54a65595
gary0958 <gljfb@yahoo.com>
2019-08-05 13:48:13 -0400

    Add transactions
    Add Transactions to sql Data access
    and used them for sales data call.

--- GRMDataManager.Library/DataAccess/SaleData.cs
+++ GRMDataManager.Library/DataAccess/SaleData.cs
@@ -52,20 +52,36 @@ namespace GRMDataManager.Library.DataAccess
             };
 
             sale.Total = sale.SubTotal + sale.Tax;
-            //save the sale model
-            SqlDataAccess sql = new SqlDataAccess();
-            sql.SaveData("dbo.spSale_Insert", sale, "GRMData");
 
-            //get the ID from the sale model
-            sale.Id = sql.LoadData<int, dynamic>("spSale_Lookup", new { sale.CashierId, sale.SaleDate }, "GRMData").FirstOrDefault();
-
-            //Finish filling in the saledetail models
-            foreach (var item in details)
+            
+            using (SqlDataAccess sql = new SqlDataAccess())
             {
-                item.SaleId = sale.Id;
-                //Save the sle detail models
-                sql.SaveData("dbo.spSaleDetail_Insert", item, "GRMData");
-            }          
+                try
+                {
+                    sql.StartTransaction("GRMData");
+                    //save the sale model
+                    sql.SaveDataInTransaction("dbo.spSale_Insert", sale);
+
+                    //get the ID from the sale model
+                    sale.Id = sql.LoadDataInTransaction<int, dynamic>("spSale_Lookup", new { sale.CashierId, sale.SaleDate }).FirstOrDefault();
+
+                    //Finish filling in the saledetail models
+                    foreach (var item in details)
+                    {
+                        item.SaleId = sale.Id;
+                        //Save the sle detail models
+                        sql.SaveDataInTransaction("dbo.spSaleDetail_Insert", item);
+                    }
+
+                    sql.CommitTransaction();
+                }
+                catch 
+                {
+                    sql.RollBackTransaction();
+                    throw;
+                }
+
+            }
             
         }
         //public List<ProductModel> GetProducts()
--- GRMDataManager.Library/Internal/DataAccess/SqlDataAccess.cs
+++ GRMDataManager.Library/Internal/DataAccess/SqlDataAccess.cs
@@ -10,7 +10,7 @@ using System.Threading.Tasks;
 
 namespace GRMDataManager.Library.Internal.DataAccess
 {
-    internal class SqlDataAccess
+    internal class SqlDataAccess : IDisposable
     {
         public string GetConnectinString(string name)
         {
@@ -40,5 +40,55 @@ namespace GRMDataManager.Library.Internal.DataAccess
                     commandType: CommandType.StoredProcedure);
             }
         }
+
+        private IDbConnection _connection;
+        private IDbTransaction _transaction;
+
+        public void StartTransaction(string connectionStringName)
+        {
+            string connectionString = GetConnectinString(connectionStringName);
+            _connection = new SqlConnection(connectionString);
+            _connection.Open();
+            _transaction = _connection.BeginTransaction();
+        }
+
+        public List<T> LoadDataInTransaction<T, U>(string storedProcedure, U parameters)
+        {
+            List<T> rows = _connection.Query<T>(storedProcedure, parameters,
+                commandType: CommandType.StoredProcedure, transaction: _transaction).ToList();
+
+            return rows;
+            
+        }
+
+        public void SaveDataInTransaction<T>(string storedProcedure, T parameters)
+        {    
+            _connection.Execute(storedProcedure, parameters,
+                commandType: CommandType.StoredProcedure, transaction: _transaction);
+            
+        }
+
+        public void CommitTransaction()
+        {
+            _transaction?.Commit();
+            _connection?.Close();
+        }
+
+        public void RollBackTransaction()
+        {
+            _transaction?.Rollback();
+            _connection?.Close();
+        }
+
+        public void Dispose()
+        {
+            CommitTransaction();
+            //throw new NotImplementedException();
+        }
+        //Open connect/ start transaction method
+        //load using the transaction
+        //save using the transaction
+        //Close connection/ stop transaction method
+        //Dispose
     }
 }
